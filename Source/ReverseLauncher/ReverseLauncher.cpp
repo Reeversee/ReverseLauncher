16a5893c012fc8df3e73b6b4d5b28be6c9f92eef
ミーシャ-チェリー <72266338+bublickkk@users.noreply.github.com>
2023-09-24 20:19:09 +0300

    Switching to the new version of Launcher №2

---
+++ Source/ReverseLauncher/ReverseLauncher.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ReverseLauncher.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ReverseLauncher, "ReverseLauncher" );
